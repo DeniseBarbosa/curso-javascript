53b19d91a3f260ca96334258886d9aa30654b0ce
DeniseBarbosa <deniseluvoir@gmail.com>
2023-08-20 11:16:27 -0300

    documentação simples do código

--- ex-funcao-arrays-objetos/script.js
+++ ex-funcao-arrays-objetos/script.js
@@ -5,13 +5,15 @@ function meuEScopo( ) {
   const pessoas = [];
 
   function eventoFormulario(evento) {
-    // evita que o envio padrão de enviar formulario (desativa) 
-    evento.preventDefault();
+ 
+    evento.preventDefault();  // Impede o envio padrão do formulário
+    // Seleciona os campos de entrada do formulário
     const nome = form.querySelector('.nome');
     const sobrenome = form.querySelector('.sobrenome');
     const peso = form.querySelector('.peso');
     const altura = form.querySelector('.altura');
     
+    // Cria um objeto com os valores dos campos e adiciona-o ao array 'pessoas'
     pessoas.push({
       nome: nome.value,
       sobrenome: sobrenome.value,
@@ -19,10 +21,15 @@ function meuEScopo( ) {
       altura: altura.value
     });
 
+    // Exibe as informações no console e na área de resultado
     console.log(pessoas);
     resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} Peso => ${peso.value} Altura => ${altura.value}</p>`
 
   };
+  // Adiciona o evento de envio de formulário ao formulário
   form.addEventListener('submit', eventoFormulario);
 };
+// Chama a função para configurar o escopo
 meuEScopo();
+
+// Resumindo, este código configura um formulário que captura o nome, sobrenome, peso e altura de uma pessoa, armazena essas informações em um array e exibe os detalhes no console e em uma área de resultado no HTML, sem realizar o envio padrão do formulário.
