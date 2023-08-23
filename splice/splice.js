8bc433fc4bcf8545105d11b59d589259befd7e69
DeniseBarbosa <deniseluvoir@gmail.com>
2023-08-23 08:19:00 -0300

    splice

---
+++ splice/splice.js
@@ -0,0 +1,4 @@
+const frutas = ["Maçã", "Banana", "Laranja", "Uva", "Morango", "Abacaxi", "Pera", "Kiwi", "Manga", "Melancia"];
+// remover elemento comeã com indice-> termina com elemento
+const remover = frutas.splice(4,2) //morango,abacaxi
+console.log(frutas, remover);
